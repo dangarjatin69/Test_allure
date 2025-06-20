76fc64198d5f3921e407bba6f9fadb3435dda1a5
Jatindangar <jatin.dangar@rishvi.co.uk>
2025-06-20 13:13:57 +0530

    Create test_allure.py

---
+++ test_allure.py
@@ -0,0 +1,13 @@
+# test_simple_allure.py
+
+import pytest
+import allure
+
+@allure.feature("Example Feature")
+@allure.story("Simple arithmetic validation")
+def test_addition():
+    with allure.step("Compute 1 + 1"):
+        result = 1 + 1
+
+    with allure.step("Verify the result is 2"):
+        assert result == 2
